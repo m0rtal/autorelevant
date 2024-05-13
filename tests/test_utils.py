417b118d29463d5aee8a862faf1a9c8f7792b0b9
jollyreap <leonid1988mail@mail.ru>
2024-05-14 01:25:07 +0300

    тест для гугла

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import pytest
 import pytest_asyncio
 
 import asyncio
-from utils import *
+
 
 
 # Пока для теста запускаю здесь
@@ -13,6 +13,20 @@ async def test_yandex(yandex_response):
     ''' Проверяет работу API для яндекса '''
 
     response, code = await yandex_response
+
+    # проверка статуса ответа
+    assert code == 200
+    # проверка на тип возвращаемых данных
+    assert isinstance(response, dict)
+    # проверка наличия главной метрики - lsi
+    assert len(response['lsi']) > 0
+
+
+@pytest.mark.asyncio
+async def test_google(google_response):
+    ''' Проверяет работу API для яндекса '''
+
+    response, code = await google_response
 
     # проверка статуса ответа
     assert code == 200
